326477232f2016109b545dac26680a39016c3e76
hotmix <dai@hotmix.jp>
2018-06-06 11:40:51 +0900

    add menuList

--- app/src/main/java/jp/hotmix/intentsample/MenuListActivity.java
+++ app/src/main/java/jp/hotmix/intentsample/MenuListActivity.java
@@ -2,6 +2,13 @@ package jp.hotmix.intentsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MenuListActivity extends AppCompatActivity {
 
@@ -9,5 +16,68 @@ public class MenuListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_list);
+
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        List<Map<String, String>> menuList = new ArrayList<>();
+
+        Map<String, String> menu = new HashMap<>();
+        menu.put("name", "から揚げ定食");
+        menu.put("price", "800円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "生姜焼き定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ステーキ定食");
+        menu.put("price", "1000円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "野菜炒め定食");
+        menu.put("price", "750円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "とんかつ定食");
+        menu.put("price", "900円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ミンチカツ定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "チキンカツ定食");
+        menu.put("price", "900円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "コロッケ定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+
+        String[] from = {"name", "price"};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                MenuListActivity.this,
+                menuList,
+                android.R.layout.simple_list_item_2,
+                from,
+                to
+        );
+
+        lvMenu.setAdapter(adapter);
+
+
     }
 }
